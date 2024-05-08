cac3634bcbfebee69b68d825820655da0747b2cf
Fu Cheng <alexcheng1982@gmail.com>
2024-05-08 08:51:04 +0800

    simplify configuration

--- src/main/java/io/github/llmagentbuilder/example/csvprocessor/CsvProcessorApplication.java
+++ src/main/java/io/github/llmagentbuilder/example/csvprocessor/CsvProcessorApplication.java
@@ -1,9 +1,13 @@
 package io.github.llmagentbuilder.example.csvprocessor;
 
+import io.github.llmagentbuilder.spring.dev.AgentDevConfiguration;
+import io.github.llmagentbuilder.spring.spring.agentcontroller.AgentController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({AgentDevConfiguration.class, AgentController.class})
 public class CsvProcessorApplication {
 
   public static void main(String[] args) {
