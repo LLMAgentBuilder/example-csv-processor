498e5e52802917d97b8450245414ae1c5c2ed4fe
Fu Cheng <alexcheng1982@gmail.com>
2024-05-08 16:47:18 +0800

    Use AgentControllerConfiguration

--- src/main/java/io/github/llmagentbuilder/example/csvprocessor/CsvProcessorApplication.java
+++ src/main/java/io/github/llmagentbuilder/example/csvprocessor/CsvProcessorApplication.java
@@ -1,13 +1,13 @@
 package io.github.llmagentbuilder.example.csvprocessor;
 
 import io.github.llmagentbuilder.spring.dev.AgentDevConfiguration;
-import io.github.llmagentbuilder.spring.spring.agentcontroller.AgentController;
+import io.github.llmagentbuilder.spring.spring.agentcontroller.AgentControllerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({AgentDevConfiguration.class, AgentController.class})
+@Import({AgentDevConfiguration.class, AgentControllerConfiguration.class})
 public class CsvProcessorApplication {
 
   public static void main(String[] args) {
