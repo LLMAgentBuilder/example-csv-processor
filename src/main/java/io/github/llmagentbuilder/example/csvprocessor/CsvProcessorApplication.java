d7f5936f0f11ad2c2b2d84325a8b91df8931cce7
Fu Cheng <alexcheng1982@gmail.com>
2024-05-11 12:21:41 +0800

    update with LLM Agent Builder

--- src/main/java/io/github/llmagentbuilder/example/csvprocessor/CsvProcessorApplication.java
+++ src/main/java/io/github/llmagentbuilder/example/csvprocessor/CsvProcessorApplication.java
@@ -1,7 +1,7 @@
 package io.github.llmagentbuilder.example.csvprocessor;
 
+import io.github.llmagentbuilder.spring.agentcontroller.AgentControllerConfiguration;
 import io.github.llmagentbuilder.spring.dev.AgentDevConfiguration;
-import io.github.llmagentbuilder.spring.spring.agentcontroller.AgentControllerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
