6e184f1613373fdc062beeb7771741845cc64603
Fu Cheng <alexcheng1982@gmail.com>
2024-10-08 21:54:40 +1300

    update to new agent

---
+++ src/main/java/io/github/llmagentbuilder/example/csvprocessor/AppMain.java
@@ -0,0 +1,10 @@
+package io.github.llmagentbuilder.example.csvprocessor;
+
+import io.github.llmagentbuilder.bootstrap.AgentMain;
+
+public class AppMain {
+
+  public static void main(String[] args) {
+    AgentMain.INSTANCE.start(null);
+  }
+}
